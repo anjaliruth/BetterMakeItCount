c450f6f628f5e9036842177344597da0fedf4ec0
anjaliruth <ruthanjali0211@gmail.com>
2023-06-19 13:33:25 +0800

    addes time fir each log

--- myapp/src/components/TimeTracker/TimeTracker.js
+++ myapp/src/components/TimeTracker/TimeTracker.js
@@ -6,17 +6,22 @@ export default function TimeTracker() {
   const [todayTotal, setTodayTotal] = useState(0);
   const [elapsedTimes, setElapsedTimes] = useState([]);
   const [sessionsByDates, setSessionsByDates] = useState({});
+  const [sessionTimeStamp, setSessionTimeStamp] = useState([]);
   const currentDate = new Date().toISOString().split("T")[0];
+  // const currentTime = DateTime.now().toFormat("[HH:mm]: ");
   //   console.log("start:",startTimeStamp);
   //   console.log("session:",elapsedTimes);
   // console.log("Dates:", sessionsByDates);
   // console.log("elapsedTime", elapsedTimes);
   // console.log("Today total", todayTotal);
+  console.log("Sessions for Current Date:", sessionsByDates[currentDate]);
+  console.log("Sessions by Dates:", sessionsByDates);
+  // console.log("time", currentTime);
+  localStorage.clear()
 
   //   console.log(new Date())
 
-
-  const daysOfTheWeek = [1,2,3,4,5,6,7]
+  const daysOfTheWeek = [1, 2, 3, 4, 5, 6, 7];
   const useRenderOnInterval = (interval) => {
     const [, setter] = useState(0);
     function rerender() {
@@ -37,10 +42,8 @@ export default function TimeTracker() {
       try {
         const parsedSession = JSON.parse(storedSession);
         setSessionsByDates(parsedSession);
-
-
+        console.log("Storage:", parsedSession);
         
-        console.log("Storage:",parsedSession )
       } catch (error) {
         console.error("Error parsing sessions from local storage");
       }
@@ -75,20 +78,25 @@ export default function TimeTracker() {
   }
 
   function handleStop() {
-    const newData = { "2023-06-15": [32334] };
+    const newData = { "2023-06-15": {elapsedtime: 32334, } };
+    const stopTime = DateTime.now().toFormat("[h:mm a]: ");
     setElapsedTimes((prevTotal) => {
       const updatedElapsedTimes = [...prevTotal, Date.now() - startTimeStamp];
       setSessionsByDates((prev) => {
         const updatedSessions = { ...prev };
         if (updatedSessions[currentDate]) {
-          updatedSessions[currentDate].push(Date.now() - startTimeStamp);
+          updatedSessions[currentDate].push({elapsedTime: Date.now() - startTimeStamp, timestamp: stopTime});
         } else {
-          updatedSessions[currentDate] = [Date.now() - startTimeStamp];
+          updatedSessions[currentDate] = [
+  ...(updatedSessions[currentDate] || []),
+  { elapsedTime: Date.now() - startTimeStamp, timestamp: stopTime }
+];
+
         }
 
         //we return these 2 because the state stters have been called for these 2 states, which trigger a rerender.
         //by returning updatedSession and updatedElapsedTimes, we make sure that  subsequent code that depends on those state values will have access to the most recent and accurate data.
-// // Adds new data to localStorage
+        // // Adds new data to localStorage
         Object.assign(updatedSessions, newData);
         localStorage.setItem("session", JSON.stringify(updatedSessions));
         setTodayTotal(totalDaily(currentDate));
@@ -100,10 +108,18 @@ export default function TimeTracker() {
     setStartTimeStamp(null);
   }
 
+  function handleDelete(date, index) {
+    setSessionsByDates((prev) => {
+      const updatedSessions = { ...prev };
+      if (updatedSessions[date]) {
+        updatedSessions[date].splice(index, 1);
+      }
+    });
+  }
   function totalDaily(date) {
-    if (sessionsByDates[date]) {
+    if (sessionsByDates[date]&& Array.isArray(sessionsByDates[date])) {
       return sessionsByDates[date].reduce(
-        (total, duration) => total + duration,
+        (total, duration) => total + duration.elapsedTime,
         0
       );
     } else {
@@ -112,11 +128,11 @@ export default function TimeTracker() {
   }
 
   function getLastWeekDates(length) {
-    let currentDate = new Date()
-let startDate = new Date(currentDate)
-    startDate.setDate(startDate.getDate() - length)
-//convert to ISO so that DateTime luxon can format it fromISO to ''dd LLL yy'
-    return startDate.toISOString().split("T")[0]
+    let currentDate = new Date();
+    let startDate = new Date(currentDate);
+    startDate.setDate(startDate.getDate() - length);
+    //convert to ISO so that DateTime luxon can format it fromISO to ''dd LLL yy'
+    return startDate.toISOString().split("T")[0];
   }
 
   return (
@@ -128,22 +144,39 @@ let startDate = new Date(currentDate)
       <button className="stop" onClick={handleStop}>
         Stop
       </button>
-      
+
       {sessionsByDates[currentDate] && <h2>Today</h2>}
       {sessionsByDates[currentDate] &&
         sessionsByDates[currentDate].map((singleSession, i) => (
-          //rescale => rescale units to their largest representation
-          <h1 className="elapsedTime" key={i}>
-            {msRescale(singleSession)}
-          </h1>
+          //rescale => rescale units to their largest representation.
+          <div className="">
+          <h2 className="elapsedTime" key={i}>
+              {singleSession.timestamp}
+            </h2>
+            <h1 className="elapsedTime" key={i}>
+              {msRescale(singleSession.elapsedTime)}
+            </h1>
+            <button
+              className="deleteButton"
+              onClick={() => handleDelete(currentDate, i)}
+            >
+              Delete
+            </button>
+          </div>
         ))}
       {todayTotal > 0 && <h1>Total: {msRescale(todayTotal)}</h1>}
-      {daysOfTheWeek.map((day, i)=>
-      <div>
-      <h2 key={i}>{DateTime.fromISO(getLastWeekDates(day)).toFormat('dd LLL yy')}</h2>
-      <h3 key={i+100}>{msRescale(totalDaily(getLastWeekDates(day)))}</h3>
-      </div>)
-      }
+      {daysOfTheWeek.map((day, i) => (
+        <div>
+          <h2 key={i}>
+            {DateTime.fromISO(getLastWeekDates(day)).toFormat("dd LLL yy")}
+          </h2>
+          <h3 key={i + 100}>
+            {totalDaily(getLastWeekDates(day))
+              ? msRescale(totalDaily(getLastWeekDates(day)))
+              : "No recorded sessions"}
+          </h3>
+        </div>
+      ))}
     </div>
   );
 }
